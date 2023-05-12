087a9479fd39997a0805b616d0f645f3a6030139
chr1s <61233068+invictus737@users.noreply.github.com>
2023-05-13 00:06:34 +0300

    Update cooler_controller.sh

--- cooler_controller.sh
+++ cooler_controller.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 echo "Fan controll started"
-gpio mode 7 out
+gpio mode 2 out
 
 while true
 do
 temperature=` cat /etc/armbianmonitor/datasources/soctemp`
 # Upper edge
-if [ $temperature -gt 47000 ]; then
+if [ $temperature -gt 55000 ]; then
   echo "Fan ON"
-  gpio write 7 1
+  gpio write 2 1
 fi;
 
 # Lower edge
-if [ $temperature -lt 45000 ]; then
+if [ $temperature -lt 50000 ]; then
   echo "Fan OFF"
-  gpio write 7 0
+  gpio write 2 0
 fi
 sleep 3
 done
